7429a314bdb7f36fefb4ee1247256a9125e954b2
alantonov <a.antonov@cft.ru>
2019-05-14 15:41:12 +0700

    Added layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCardModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { PlaygroundComponent } from './playground/playground/playground.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PlaygroundComponent
-  ],
+  declarations: [AppComponent, PlaygroundComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/playground/playground/playground.component.ts
+++ src/app/playground/playground/playground.component.ts
@@ -1,14 +1,68 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { MatCardModule } from '@angular/material/card';
+
+export interface Ship {
+  life: number;
+  isAlive: boolean;
+  size: number;
+  shape?: number;
+  x: number;
+  y: number;
+}
+
+export interface Row {
+  cells: number[];
+}
+
+export interface Playground {
+  cells: number[][];
+  ships?: Ship[];
+  score?: number;
+  ships_left?: number;
+}
+
+export const CLASSES = {
+  sea: 0,
+  ship: 1
+};
 
 @Component({
-  selector: "app-playground",
-  templateUrl: "./playground.component.html",
-  styleUrls: ["./playground.component.css"]
+  selector: 'app-playground',
+  templateUrl: './playground.component.html',
+  styleUrls: ['./playground.component.css']
 })
 export class PlaygroundComponent implements OnInit {
   @Input() name: string;
+  @Input() id: number;
 
-  constructor() {}
+  dataset$: Observable<Playground>;
+  dataset: Playground = {
+    cells: [],
+    score: 0
+  };
 
-  ngOnInit() {}
+  shipsLeft = 10;
+
+  constructor() {
+    this.setPlayground();
+  }
+
+  ngOnInit() {
+    console.log(this.name);
+
+    // this.dataset$ = of(MOCK_DATASET);
+
+    console.table(this.dataset);
+  }
+
+  setPlayground() {
+    for (let i = 0; i < 10; i++) {
+      const row = [];
+      for (let j = 0; j < 10; j++) {
+        row.push(Math.floor(Math.random() * 2));
+      }
+      this.dataset.cells.push(row);
+    }
+  }
 }
